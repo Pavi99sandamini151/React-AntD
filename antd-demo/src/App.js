2d0842bd5e35bbb9f2e5dcb3bf8be1613458c9e2
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-12-03 13:25:03 +0530

    initial commit

---
+++ antd-demo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'antd/dist/reset.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button type="primary">My first</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
