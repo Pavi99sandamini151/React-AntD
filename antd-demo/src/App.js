6bda93700df96f1a76f54979777f5e7c97055826
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-12-03 13:48:42 +0530

    initial commit

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import 'antd/dist/reset.css';
 import { Button } from 'antd';
+import {useState} from "react";
+import { PoweroffOutlined } from '@ant-design/icons';
 
 function App() {
+    const [loading,setLoading] = useState(false);
+    const onButtonClick = (e) => {
+        console.log("hello pavi");
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <Button type="primary">My first</Button>
+        <Button type="primary"
+                block
+                loading={loading}
+                style={{backgroundColor:'orange', color:'red'}}
+                icon={<PoweroffOutlined/>}
+                onClick={onButtonClick}>
+            My first
+        </Button>
       </header>
     </div>
   );
