199dacda738603796dc104ba42cf4b379ce3213e
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-12-03 19:16:49 +0530

    initial commit

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,116 +1,21 @@
 import './App.css';
 import 'antd/dist/reset.css';
-import { Input, Select, Form, Button, Table } from "antd";
+import { Button, Progress, Spin } from 'antd';
 import { useState } from "react";
-import { PoweroffOutlined, UserOutlined } from '@ant-design/icons';
+
 
 function App() {
-    const [loading,setLoading] = useState(false);
-    const fruits = ['Banana', 'Mango', 'Orange', 'Chery'];
-    const data = [
-    {
-        name:'Name 1',
-        age:10,
-        address:'address 1',
-        key:1
-    },
-    {
-        name:'Name 2',
-        age:10,
-        address:'address 2',
-        key:2
-    },
-    {
-        name:'Name 3',
-        age:10,
-        address:'address 3',
-        key:3
-    }
-    ];
-    const columns = [
-        {
-            title:'Name',
-            dataIndex:'name',
-            key:'key',
-            render:name => {
-                return <a>{name}</a>
-            }
-        },
-        {
-            title:'Age',
-            dataIndex:'age',
-            key:'key'
-        },
-        {
-            title:'Address',
-            dataIndex:'address',
-            key:'key'
-        },
-    ];
-    const onButtonClick = (e) => {
-        console.log("hello pavi");
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }
-    const onFinish = (e) => {
-        console.log(e);
-    }
+  const [loading,setLoading] = useState(false);
   return (
     <div className="App">
-        <Button type="primary"
-                loading={loading}
-                style={{backgroundColor:'orange', color:'red'}}
-                icon={<PoweroffOutlined/>}
-                onClick={onButtonClick}>
-            My first
-        </Button>
-          <Input.Search
-                 placeholder="Enter Name"
-                 maxLength={10}
-                 type="text"
-                 prefix={<UserOutlined/>}
-                 allowClear>
-          </Input.Search>
-          <Select
-              placeholder="Select fruit"
-              mode="multiple"
-              style={{width:'50%'}}>
-              { fruits.map((fruit,index)=>{
-                  return <Select.Option key={index} value={fruit}>{fruit}</Select.Option>
-              })}
-          </Select>
-          <div style={{width:'40%', justifyContent:'center'}}>
-              <Form onFinish={onFinish}>
-                  <Form.Item label="User Name">
-                      <Input placeholder="User Name"></Input>
-                  </Form.Item>
-                  <Form.Item label="User Address">
-                      <Input placeholder="User Address"></Input>
-                  </Form.Item>
-                  <Form.Item label="User Password">
-                      <Input.Password placeholder="User Password"></Input.Password>
-                  </Form.Item>
-                  <Form.Item label="User Phone Number">
-                      <Input placeholder="User Phone Number"></Input>
-                  </Form.Item>
-                  <Form.Item>
-                      <Button
-                          type="primary"
-                          htmlType="submit"
-                          block>
-                          Log In
-                      </Button>
-                  </Form.Item>
-              </Form>
-          </div>
-        <div style={{width:'50%'}}>
-            <Table dataSource={data}
-                   columns={columns}
-                   pagination>
-            </Table>
-        </div>
+        <Spin spinning={loading}></Spin>
+        <Button onClick={()=>{setLoading(preValue => !preValue)}}>Toggle Spinning</Button>
+        <Spin spinning={loading}>
+            <p>P tag 1</p>
+            <p>P tag 2</p>
+            <p>P tag 3</p>
+        </Spin>
+        <Progress percent={37}/>
     </div>
   );
 }
